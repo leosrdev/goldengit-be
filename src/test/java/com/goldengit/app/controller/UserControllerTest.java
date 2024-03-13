2ea848afbd893a3c5b0aa2bc933f830c294db19e
Leo Campos <leosrdev@gmail.com>
2024-03-12 19:48:24 -0600

    Cleaning imports

--- src/test/java/com/goldengit/app/controller/UserControllerTest.java
+++ src/test/java/com/goldengit/app/controller/UserControllerTest.java
@@ -1,24 +1,11 @@
 package com.goldengit.app.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.goldengit.app.dto.UserRequest;
 import com.goldengit.app.dto.UserResponse;
-import com.goldengit.app.repository.UserRepository;
-import com.goldengit.app.service.UserService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
