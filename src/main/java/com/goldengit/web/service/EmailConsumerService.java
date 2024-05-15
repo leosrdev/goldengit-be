f8d0839ea6e5e8737d688fc82ea6f0f3371fdc06
Leo Campos <leosrdev@gmail.com>
2024-05-15 14:10:07 -0600

    Adding YAML config with email settings

--- src/main/java/com/goldengit/web/service/EmailConsumerService.java
+++ src/main/java/com/goldengit/web/service/EmailConsumerService.java
@@ -1,5 +1,6 @@
 package com.goldengit.web.service;
 
+import com.goldengit.web.config.EmailConfig;
 import com.goldengit.web.dto.EmailMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -14,20 +15,22 @@ public class EmailConsumerService {
     @Autowired
     private EmailSenderService emailSenderService;
 
+    @Autowired
+    private EmailConfig emailConfig;
+
     @Value("${goldengit.api.domain}")
     private String domain;
-    @Value("${account.activation.email.from}")
-    private String from;
-    @Value("${account.activation.email.subject}")
-    private String subject;
-    @Value("${account.activation.email.text}")
-    private String text;
 
     @RabbitListener(queues = "${rabbitmq.email.queue}")
     public void processMessage(EmailMessage emailMessage) {
         String link = String.format("https://%s/activate?t=%s", domain, emailMessage.getActivationToken());
-        String formattedText = String.format(text, emailMessage.getName(), link);
-        emailSenderService.sendSimpleMessage(from, emailMessage.getEmail(), subject, formattedText);
+        String formattedText = String.format(emailConfig.getEmailText(), emailMessage.getName(), link);
+        emailSenderService.sendSimpleMessage(
+                emailConfig.getEmailFrom(),
+                emailMessage.getEmail(),
+                emailConfig.getEmailSubject(),
+                formattedText
+        );
         log.info(String.format("Email sent to: %s", emailMessage.getEmail()));
     }
 }
