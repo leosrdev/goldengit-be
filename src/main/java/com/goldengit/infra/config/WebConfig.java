657075b66ce4eaaae2f50766e0aa450c474a0e53
Leo Campos <leosrdev@gmail.com>
2024-06-17 10:15:49 -0600

    Updating CORS and port numbers for dev/prod

--- src/main/java/com/goldengit/infra/config/WebConfig.java
+++ src/main/java/com/goldengit/infra/config/WebConfig.java
@@ -13,7 +13,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "goldengit.com")
+                .allowedMethods("GET")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 
     @Bean
